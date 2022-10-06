d5f6b76c6578b2763420d637d9769e90357d178f
Felipe Barboza <felipebarboza5@gmail.com>
2022-10-06 12:15:33 -0300

    :construction: feat: login front

--- app/frontend/src/pages/Login.js
+++ app/frontend/src/pages/Login.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import loginLogo from '../images/loginLogo.png';
+import SIX from '../utils/numbers';
 import './login.css';
 
 export default function Login() {
+  const [data, setData] = useState({});
+  const [disabled, setDisabled] = useState(true);
+  const { email, password } = data;
+
+  const handleInput = ({ target: { name, value } }) => {
+    setData({ ...data, [name]: value });
+  };
+
+  useEffect(() => {
+    if (email && password) {
+      const emailReg = /\S+@\S+\.\S+/;
+      if (email.match(emailReg) && password.length >= SIX) {
+        setDisabled(false);
+      } else {
+        setDisabled(true);
+      }
+    }
+  }, [email, password]);
+
   return (
     <div className="form__group">
       <img id="login_logo" src={ loginLogo } alt="login logo" />
@@ -10,20 +30,23 @@ export default function Login() {
         type="email"
         className="form__field"
         placeholder="email@email.com"
+        name="email"
         required
-        id="email"
+        onChange={ (e) => handleInput(e) }
       />
       <input
         type="password"
         placeholder="********"
         className="form__field"
+        name="password"
         required
-        id="password"
+        onChange={ (e) => handleInput(e) }
       />
       <br />
       <button
         type="button"
         className="btn draw-border"
+        disabled={ disabled }
       >
         Login
       </button>
