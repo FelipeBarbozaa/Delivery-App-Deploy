f2e257fa1e644f9efcf303b243dc877a559689bb
Felipe Barboza <felipebarboza5@gmail.com>
2022-10-12 09:51:29 -0300

    :wrench: feat: cors

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 import userRouter from './routes/UserRouter';
 import errorHandler from './middlewares/error';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 app.use('/', userRouter);
 
 app.use(errorHandler);
