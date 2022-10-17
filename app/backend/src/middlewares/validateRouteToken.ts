a19c08ffffa851bd8e99e7be8966764cfa8560e6
Felipe Barboza <felipebarboza5@gmail.com>
2022-10-17 10:06:49 -0300

    :sparkles: feat: Token route validate created

---
+++ app/backend/src/middlewares/validateRouteToken.ts
@@ -0,0 +1,36 @@
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+import User from '../database/models/User';
+
+const { JWT_SECRET } = process.env; 
+
+// eslint-disable-next-line max-lines-per-function
+const validateRouteToken = async (
+  req: Request, res: Response, next: NextFunction
+  ) => {
+  try {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+      return res.status(401).json({ message: 'Token not found' });
+    }
+    
+    const decoded = jwt.verify(
+      authorization, JWT_SECRET as string) as JwtPayload;
+
+    if (!decoded) {
+      return res.status(401).json({ message: 'Invalid or expired token' });
+    }
+
+    const userIsValid = await User.findOne({ where: { email: decoded.email }});
+    if (!userIsValid) {
+      return res.status(401).json({ message: 'Invalid or expired token' });
+    }
+
+    next();
+  } catch (error) {
+    return res.status(444).json(error);
+  }
+};
+
+export default validateRouteToken;
--- app/backend/src/routes/ProductRouter.ts
+++ app/backend/src/routes/ProductRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import ProductFactory from '../factories/ProductFactory';
+import validateRouteToken from '../middlewares/validateRouteToken';
 
 const router = Router();
 
-router.get('/products', (req, res) => ProductFactory().getProducts(req, res));
+router.get('/products', validateRouteToken,
+  (req, res) => ProductFactory().getProducts(req, res));
 
 export default router;
