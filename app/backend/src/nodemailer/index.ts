dd7a44631755d683a36e9a7dbdaff38a7a9f9d02
Felipe Barboza <felipebarboza5@gmail.com>
2022-11-04 15:13:53 -0300

    fix lint

--- app/backend/src/nodemailer/index.ts
+++ app/backend/src/nodemailer/index.ts
@@ -1,7 +1,8 @@
 import transporter from './config';
 
 const sendEmail = async (email: string, token: string) => {
-  const url = `https://delivery-app-deploy-production.up.railway.app//confirmation/${token}`;
+  const url = `https://delivery-app-deploy-production.up.railway.app/
+    confirmation/${token}`;
 
   await transporter.sendMail({
     subject: 'Confirm your email',
